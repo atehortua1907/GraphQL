5b9ad7b49454bc54fa6376fb32ba411dd21bed37
David Atehortua <d.atehortua@sana-commerce.com>
2021-11-19 17:39:42 -0500

    finished training

--- 2-DanielDiazYoutube/2-Code/TestGraphQLWithHotChocolate/TestGraphQLWithHotChocolate/Startup.cs
+++ 2-DanielDiazYoutube/2-Code/TestGraphQLWithHotChocolate/TestGraphQLWithHotChocolate/Startup.cs
@@ -1,59 +1,107 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HotChocolate;
+using HotChocolate.AspNetCore;
+using HotChocolate.Subscriptions;
+using HotChocolate.Types;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace TestGraphQLWithHotChocolate
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
             services.AddDbContext<ApplicationDbContext>(option =>
                 option.UseSqlServer("Data Source=DESKTOP-7OC0NID\\SQLEXPRESS;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;Database=DemoGraphQL"));
+
+            services.AddInMemorySubscriptions();
+            services.AddGraphQL(sp => SchemaBuilder.New()
+                .AddQueryType<Query>()
+                .AddMutationType<Mutation>()
+                .AddSubscriptionType<Subscription>()
+                .AddServices(sp)
+                .Create());
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-            });
+            app.UseWebSockets();
+            app.UseGraphQL();
         }
+
+        public class Query
+        {
+            [UseSelection]
+            [UseFiltering]
+            [UseSorting]
+            public IEnumerable<Opportunity> GetOpportunitities([Service] ApplicationDbContext context)
+                    => context.Opportunities;
+        }
+
+        public class Mutation
+        {
+
+            public async Task<Guid> CreateAccount(string name, [Service] ApplicationDbContext context)
+            {
+                var account = new Account()
+                {
+                    Name = name
+                };
+                context.Accounts.Add(account);
+                await context.SaveChangesAsync();
+                return account.Id;
+            }
+
+            public async Task<Guid> CreateOpportunity(
+                string subject,
+                Guid accountId,
+                Guid userId,
+                [Service] ApplicationDbContext context,
+                [Service] ITopicEventSender topicEventSender)
+            {
+                var opportunity = new Opportunity()
+                {
+                    AccountId = accountId,
+                    Subject = subject,
+                    OwnerId = userId
+                };
+                context.Opportunities.Add(opportunity);
+                await context.SaveChangesAsync();
+                await topicEventSender.SendAsync("opportunities", opportunity.Id.ToString());
+                return opportunity.Id;
+            }
+        }
+
+        public class Subscription
+        {
+
+            [SubscribeAndResolve]
+            public async ValueTask<IAsyncEnumerable<string>> OnOpportunityCreated([Service] ITopicEventReceiver topicEventReceiver)
+            {
+                return await topicEventReceiver.SubscribeAsync<string, string>("opportunities");
+            }
+
+            [SubscribeAndResolve]
+            public async IAsyncEnumerable<string> OnMessagesAsync()
+            {
+                yield return "1";
+                await Task.Delay(2000);
+                yield return "2";
+                await Task.Delay(2000);
+                yield return "3";
+                await Task.Delay(2000);
+                yield return "4";
+            }
+        }
+
     }
 }
